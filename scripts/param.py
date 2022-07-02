5b8a78f471d98f696d901b0ccb5ed84b4e2db83b
Makiu <gabrielkoh99@gmail.com>
2022-07-02 17:18:37 +0200

    correcciones de bugs por refactorización

---
+++ scripts/param.py
@@ -0,0 +1,20 @@
+"""Parámetros
+
+Este script contiene los parámetros empleados en la reconstrucción
+"""
+
+import numpy
+
+param = {'theta':numpy.pi/3,    #rad
+         'p': 0.3,              #cm
+         'v': 0.156267,         #cm/us
+         'v12': 0.17736,        #cm/us
+         'v23': 0.190037,       #cm/us
+         'twin': 0.2e3,         #us
+         'a1': 201.45,          #cm
+         'a2': 201.75,          #cm
+         'a3': 202.05,          #cm
+         'b': -0.0866025,       #cm
+         'c1': 4.95,            #cm
+         'c2': 5.25             #cm
+        } 
